761f0bc548d943ace440a7866b9837c059d17d29
berat <berat.ademi1000@gmail.com>
2017-12-25 20:23:03 +0100

    new try

--- rgg.cpp
+++ rgg.cpp
@@ -4,6 +4,6 @@ using namespace std;
 // main() is where program execution begins.
 int main() {
    cout << "Hello World" << endl; // prints Hello World
-   cout << "Klasse wie das läuft";
+   cout << "Klasse wie das läuft" << endl;
    return 0;
 }
