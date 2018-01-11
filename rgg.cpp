4108317260805da6496d2fcafae1f456a3c04d4e
Berat Ademi <berat.ademi1000@gmail.com>
2018-01-11 13:01:56 +0100

    first algorithm

--- rgg.cpp
+++ rgg.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <vector>
 using namespace std;
+
+
+
+void quote(){
+  char retry = ' ';
+  int linecount = 0;
+  int randQuote_int = 0;
+  string line = "";
+  vector<string>randQuote_vect;
+
+  ifstream goethe("Faust.txt");
+
+  do{
+
+      srand(time(0));
+
+      if(goethe.is_open()){
+        while(!goethe.eof())
+        {
+          getline(goethe, line);
+          if (line != " ")
+          {
+            randQuote_vect.push_back(line);
+            linecount++;
+          }
+        }
+
+        randQuote_int = rand() % linecount;
+        cout << randQuote_vect[randQuote_int] << endl;
+        goethe.close();
+        cout << randQuote_vect.size() << endl;
+      }
+
+      else{
+        cout << "Unable to open file." << endl;
+        cout << "Try again? [Y]es / [N]o" << endl;
+        cin >> retry;
+      }
+  } while (retry == 'y' || retry == 'Y');
+}
 
 // main() is where program execution begins.
 int main() {
-   cout << "Hello World" << endl; // prints Hello World
-   cout << "Klasse wie das läuft" << endl;
-   return 0;
+quote();
+return 0;
 }
