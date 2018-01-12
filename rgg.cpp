1a9755589a7eaa5a351f10bef65c8e9b30dab932
Berat Ademi <berat.ademi1000@gmail.com>
2018-01-12 21:03:07 +0100

    new text; updated functions

--- rgg.cpp
+++ rgg.cpp
@@ -6,65 +6,80 @@
 #include <vector>
 using namespace std;
 
+int linecount = 0; //count for the lines the file provides
 
+string randtxt()
+{
+  int randtxt_int = 0;
+  string texts[2] = {"Faust.txt", "KeinWesen.txt"};
+  srand(time(0)); //sets seed number for rand function
 
-void quote(){
-  char retry = ' '; //char for controling do-while
-  int linecount = 0; //count for the lines the file provides
-  int coloncount = 0; //counting colons for better looking output
-  int randQuote_int = 0; //int for random linenumber
+  randtxt_int = rand() % 2;
+
+  return texts[randtxt_int];
+}
+
+vector<string> textin(string txt)
+{
+  char retry = 'y'; //char for controling do-while
   string line = ""; //string that saves single lines from file, to pass it on to the vector
-  string output = "";
-  vector<string>randQuote_vect; //dynamic array with text; lines stored as single strings
+  vector<string>xvec;
 
-  ifstream goethe("Faust.txt"); //opens file
+    ifstream goethe(txt); // opens file
 
-  do{
-
-      srand(time(0)); //sets seed number for rand function
-
+    while(retry == 'y' || retry == 'Y')
+    {
       if(goethe.is_open()){
         while(!goethe.eof())
         {
           getline(goethe, line);
           if (line != " ")
           {
-            randQuote_vect.push_back(line);
+            xvec.push_back(line);
             linecount++;
           }
         }
-
-        randQuote_int = rand() % linecount;
-
-        output = randQuote_vect[randQuote_int];
-        for(int i = 0; i < output.length(); i++)
-        {
-          switch(output[i])
-          {
-            case ' ': if(output[i+1] == ' ')
-                      {
-                        output[i+1] = '\n';
-                      }
-                      break;
-
-            default: break;
-          }
-        }
-        cout << output << endl;
-
+        retry = 'n';
         goethe.close();
       }
-
-      else{
+      else
+      {
         cout << "Unable to open file." << endl;
         cout << "Try again? [Y]es / [N]o" << endl;
         cin >> retry;
       }
-  } while (retry == 'y' || retry == 'Y');
+    }
+      return xvec;
+}
+
+int randint()
+{
+  srand(time(0)); //sets seed number for rand function
+  int randQuote_int = 0; //int for random linenumber
+  randQuote_int = rand() % linecount;
+  return randQuote_int;
 }
 
 // main() is where program execution begins.
 int main() {
-quote();
+string output = "";
+string a = randtxt();
+vector<string>vect = textin(a);
+
+output = vect[randint()];
+for(int i = 0; i < output.length(); i++)
+{
+  switch(output[i]) //switch modifies output
+  {
+    case ' ': if(output[i+1] == ' ')
+              {
+                output[i+1] = '\n';
+              }
+              break;
+
+    default: break;
+  }
+}
+cout << output << endl;
 return 0;
 }
